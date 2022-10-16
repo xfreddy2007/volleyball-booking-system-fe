22b12bc4900b6468c99c6e236655f1b67a8877e2
xfreddy2007 <xfreddy2007@gmail.com>
2022-10-16 22:22:22 +0800

    feat: add seo meta and title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
+import Head from 'next/head';
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  return (
+    <>
+      <Head>
+        <title>排球Queue</title>
+        <meta property="og:title" content="排球Queue" />
+        <meta property="og:url" content="https://www.volleyballqueue.com/" />
+        <meta
+          property="og:image"
+          content="https://user-images.githubusercontent.com/54787518/196040501-22b0812e-4acd-4534-8759-1d09b995dcc6.jpeg"
+        />
+        <meta
+          property="og:description"
+          content="A place for volleyball lovers to arrange and book a volleyball event easy and fast."
+        />
+        <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=0" />
+        <link rel="canonical" href="https://www.volleyballqueue.com/" />
+        <link
+          href="https://user-images.githubusercontent.com/54787518/196039228-7f6d226e-c28e-4e07-8c24-40fb91c99088.png"
+          rel="icon"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
